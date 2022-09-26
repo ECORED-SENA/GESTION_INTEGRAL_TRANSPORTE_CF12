b6cb63deb5ec14da4737122ae67ff7c518a0a0ab
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2022-09-26 17:10:31 -0500

    Ajustes

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -8,17 +8,120 @@
         span 5
       h1 Planes de mejoramiento 
 
+    img.mb-4(src="@/assets/curso/tema5/img01.png")
+    p.mb-2 Los planes de mejoramiento son instrumentos con los cuales las entidades u organizaciones pueden organizar, presentar y planear las acciones o estrategias con las cuales se pretende levantar o solventar las inconformidades encontradas en los controles mencionados en el punto 4 del presente componente. 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4.col-10
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema5/img02.png")
+      .col-lg-8.col-12
+        p.mb-4 El plan de mejoramiento deberá especificar aspectos relevantes como:
+        ol.lista-ol--cuadro.mb-0
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 1
+            | Mencionar la no conformidad.
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 2
+            | Relacionar el área encargada del proceso donde se encontró la no conformidad.
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 3
+            | Establecer los responsables del proceso.
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 4
+            | Plantear las acciones con las cuales se van a subsanar las no conformidades. 
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 5
+            | Establecer los responsables de la ejecución de las acciones.
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 6
+            | Fijar las fechas de entrega y si son acciones sistemáticas, la periodicidad de ejecución.
+          li.mb-2 
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 7
+            | El soporte o evidencia que se presentará para comprobar su ejecución. 
+          li.mb-0
+            .lista-ol--cuadro__vineta(style="background-color: #ffba5b")
+              span(style="color: #12263F") 8
+            | Firma de los intervinientes en el compromiso.
+    p.mb-4 Los planes de mejoramiento normalmente tienen un formato establecido dentro de los sistemas integrados de gestión de las organizaciones o entidades, por lo tanto, se debe contemplar el análisis del procedimiento para su diligenciamiento, y ser muy responsables en guardar los soportes y evidencias de forma adecuada para su revisión posterior, la cual normalmente se realiza con una nueva auditoría trimestral, semestral o anual, según el plan de auditorías diseñado por el área encargada.
+    p.mb-0 Un plan de mejoramiento, entre otros elementos, incluye:
+
     Separador
     #t_5_1.titulo-segundo.color-acento-contenido
       h2 5.1	Acciones de mejora en la operación
 
+    .row.justify-content-center.align-items-center.mb-0
+      .col-lg-4.col-10
+        img.mb-lg-0.mb-3(src="@/assets/curso/tema5/img03.png")
+      .col-lg-8.col-10
+        p.mb-3 Una vez analizados los problemas más frecuentes en la operación del servicio de transporte, se establecen las acciones a implementar para su solución. 
+        p.mb-3 Las acciones de mejora son aquellas estrategias con las cuales se pretenden subsanar las no conformidades encontradas o halladas por el grupo auditor de control interno o externo; pueden estar enmarcadas dentro de procesos sistemáticos o acciones de una sola vez. 
+        p.mb-3 Entre las ventajas que la optimización de rutas puede aportar a una empresa de transporte, destacan las siguientes: 
+
+        TabsA.color-acento-contenido.mb-0
+          .tarjeta.p-4(style="background-color: #e6e2ee" titulo="Ahorra en costos logísticos")
+            h4 Ahorra en costos logísticos
+            p.mb-0 Si las rutas están optimizadas, los camiones recorrerán menos kilómetros y, por lo tanto, consumirán menos combustible. 
+          .tarjeta.p-4(style="background-color: #e6e2ee" titulo="Ahorra en tiempo de trabajo")
+            h4 Ahorra en tiempo de trabajo
+            p.mb-0 Una planificación manual de las rutas toma mucho tiempo de quienes las diseñan y llevan a cabo. Mediante la optimización de rutas, se puede automatizar este proceso. Además, con rutas optimizadas, los conductores no desperdician tiempo en la carretera. 
+          .tarjeta.p-4(style="background-color: #e6e2ee" titulo="Mejor satisfacción de clientes")
+            h4 Mejor satisfacción de clientes
+            p.mb-0 Se mejora la ratio de entrega de pedidos a tiempo. Además, las rutas optimizadas tendrán en cuenta las peticiones especiales de cada cliente. 
+          .tarjeta.p-4(style="background-color: #e6e2ee" titulo="Gestionar mejor flota de camiones")
+            h4 Gestionar mejor flota de camiones
+            p.mb-0 Mediante la optimización de rutas se evita usar más camiones de los necesarios, por ende, se pueden tomar decisiones de reducir la flota de vehículos si se considera necesario. 
+          .tarjeta.p-4(style="background-color: #e6e2ee" titulo="Atender a más clientes")
+            h4 Atender a más clientes
+            p.mb-0 Reduciendo el tiempo de entrega de cada pedido será posible lograr más entregas cada día. Optimizando las rutas se puede calcular cuántos pedidos puede entregar cada camión diariamente. 
     Separador
     #t_5_2.titulo-segundo.color-acento-contenido
       h2 5.2	Disminución de costos en la operación
 
+    img.mb-4(src="@/assets/curso/tema5/img04.png")
+
+    p.mb-0 En gran medida, los costos de operación de los vehículos se ven incrementados a causa de varios aspectos que deben ser cuidadosamente controlados por el área encargada de la operación, y se relacionan con:
+
     Separador
     #t_5_3.titulo-segundo.color-acento-contenido
       h2 5.3	Mejora en procesos de distribución
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-6.col-12
+        p.mb-4 Para analizar las acciones de mejora en los problemas que puedan surgir con la distribución de mercancías, es pertinente separar la distribución urbana y la rural, ya que la mayor cantidad de problemas se desarrollan por la interacción con otros usuarios en las vías.  Cada día hay más problemas en la planificación de las ciudades por la mayor densidad poblacional y la misma capacidad vial, enfrentando problemas de tráfico y contaminación.
+        p.mb-lg-0.mb-3 Dentro de este aspecto, influyen conocimientos de la investigación de operaciones, que no es más que la disciplina mediante la cual se emplean herramientas donde, de forma analítica, se entiende la dinámica de la operación, para así conocer al detalle los aspectos que la componen y cómo se pueden optimizar los recursos para lograr los objetivos a menor costo y con mayor eficiencia. 
+      .col-lg-6.col-10
+        img.mb-0(src="@/assets/curso/tema5/img05.png")
+
+    p.mb-4 Una gran fuente de información para rastrear los problemas son las quejas de los clientes y el análisis de costos minucioso; estas son algunas acciones de mejora que se pueden implementar:
+
+    SlyderF.mb-0(columnas="col-12 col-lg-4" data-aos="fade-left")
+      .tarjeta.tarjeta1.p-4.px-5
+        div.justify-content-center.d-flex.mb-5
+          img(style="width: 118px" src='@/assets/curso/tema5/img06.svg')        
+        p.text-center.mb-0 Realizar buenos ruteos para optimizar las redes de entrega y distribución. 
+      .tarjeta.tarjeta1.p-4.px-5
+        div.justify-content-center.d-flex.mb-5
+          img(style="width: 106px" src='@/assets/curso/tema5/img07.svg')        
+        p.text-center.mb-0 Capacitación constante al personal para el tratamiento de las cargas.
+      .tarjeta.tarjeta1.p-4.px-5
+        div.justify-content-center.d-flex.mb-4
+          img(style="width: 123px" src='@/assets/curso/tema5/img08.svg')        
+        p.text-center.mb-0 Implementación de herramientas que faciliten la administración de la información de costos y tiempos. 
+      .tarjeta.tarjeta1.p-4.px-5
+        div.justify-content-center.d-flex.mb-5
+          img(style="width: 112px" src='@/assets/curso/tema5/img09.svg')        
+        p.text-center.mb-0 Consolidación de las cargas y aplicación de métodos de almacenaje adecuados.
+      .tarjeta.tarjeta1.p-4.px-5
+        div.justify-content-center.d-flex.mb-5
+          img(style="width: 114px" src='@/assets/curso/tema5/img10.svg')        
+        p.text-center.mb-0 Controlar procesos operativos, para toma de decisiones adecuadas.
 
     Separador
     #t_5_4.titulo-segundo.color-acento-contenido
